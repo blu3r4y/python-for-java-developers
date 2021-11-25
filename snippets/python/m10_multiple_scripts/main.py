0042b45ba75d626e1b0ff81ae56d85a347b37a85
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2021-11-25 08:52:01 +0100

    Also explain code organization on the slides

--- snippets/python/m10_multiple_scripts/main.py
+++ snippets/python/m10_multiple_scripts/main.py
@@ -3,4 +3,4 @@ test_variable = 42
 # the Python interpreter will set the global variable __name__ for us,
 # so that we can check it here and create a so-called "main function"
 if __name__ == "__main__":
-    print("This will only run if you specifically executed this file.")
+    print("This will only run if you specifically started the interpretor on this file.")
